00e827e24f39af8226b3cb7ed784ceb217dca8a7
DaiPei <daipei@DaiPei-MacBook-Pro.local>
2018-07-10 20:05:41 +0800

    add plugin install logic

--- install.sh
+++ install.sh
@@ -10,6 +10,21 @@ ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install thefuck
+brew install thefuck
+
+# install zplug
+brew install zplug
+
+# use zplug install stuff
+zplug install
+
+# install fzf
+brew install fzf
+
 # install vim plugin
 rm -rf .vim/bundle
 mkdir -p .vim/bundle

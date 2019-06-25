343161c0222b928e0229d66f236ecf5ee57de031
daipei <daipei@hust.edu.cn>
2019-06-26 01:36:11 +0800

    update dotfile

--- dot.sh
+++ dot.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-# backup old file
+########## vim part ##########
 if [ -L $HOME/.vim.backup ]
 then
 	rm $HOME/.vim.backup
@@ -9,28 +9,33 @@ then
 	rm -rf $HOME/.vim.backup
 fi
 
+mv -f $HOME/.vimrc $HOME/.vimrc.backup
+ln -s $HOME/dotfiles/.vim $HOME/.vim
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+##############################
+
+
+########## vscode ##########
 if [ -d $HOME/Library/Application\ Support/Code/User ]
 then
 	rm -rf $HOME/Library/Application\ Support/Code/User
 	ln -s $HOME/dotfiles/vscode $HOME/Library/Application\ Support/Code/User
 fi
+############################
 
+########## ssh part ##########
 if [ -d $HOME/.ssh ]
 then
 	if [ -f $HOME/.ssh/config ]
 	then
 		mv -f $HOME/.ssh/config $HOME/.ssh/config.backup 
 	fi
-
 	ln -s $HOME/dotfiles/sshconfig $HOME/.ssh/config
 fi
+##############################
 
-
+########## zsh part ##########
 mv -f $HOME/.zshrc $HOME/.zshrc.backup
-mv -f $HOME/.vimrc $HOME/.vimrc.backup
-
-# link new file
-ln -s $HOME/dotfiles/.vim $HOME/.vim
-ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+##############################
 

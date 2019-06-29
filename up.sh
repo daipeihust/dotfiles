f4a5af0de768cbd2cb9768f77d58ef0943f0416d
daipei <daipei@celer.network>
2019-06-29 21:52:41 +0800

    update dotfile

--- up.sh
+++ up.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-cd /Users/daipei/dotfiles
+cd /Users/daipei/dotfiles &&
 
-echo `date` >> up.out
+echo `date` >> up.out &&
 
-git pull >> up.out
-git add --all >> up.out
-git commit -m "update dotfile" >> up.out
-git push >> up.out
+git pull >> up.out &&
+git add --all >> up.out &&
+git commit -m "update dotfile" >> up.out &&
+git push >> up.out &&
 
-echo 'finish\n\n\n' >> up.out
+echo 'finish\n\n\n' >> up.out &&

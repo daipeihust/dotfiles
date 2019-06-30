d062953d2fe1900f5ca321e748361e8d73600021
daipei <daipei@celer.network>
2019-06-30 20:48:53 +0800

    update dotfile

---
+++ sync.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+while :
+do
+	cd /Users/daipei/dotfiles &&
+
+	echo `date` >> sync.out &&
+
+	git pull >> sync.out &&
+	git add --all >> sync.out &&
+	git commit -m "update dotfile" >> sync.out &&
+	git push >> sync.out
+
+	echo 'finish\n\n\n' >> sync.out
+	sleep 5
+done

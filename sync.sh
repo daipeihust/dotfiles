df7d39667e2cf59f1fbb97993ac23230121ed261
System Administrator <root@daipeideMacBook-Pro.local>
2019-07-04 01:03:23 +0800

    update dotfile

--- sync.sh
+++ sync.sh
@@ -4,13 +4,13 @@ while :
 do
 	cd /Users/daipei/dotfiles
 
-	echo `date` >> sync.info
+	echo `date` &>> sync.info
 
-	git pull >> sync.info
+	git pull &>> sync.info
 	git add --all
-	git commit -m "update dotfile" >> sync.info
-	git push >> sync.info
+	git commit -m "update dotfile" &>> sync.info
+	git push &>> sync.info
 
-	echo 'finish\n\n\n' >> sync.info
-	sleep 10
+	echo 'finish\n\n\n' &>> sync.info
+	sleep 3600
 done

58cbda6d6058f95eaddf79de08387838c973b490
daipei <daipei@celer.network>
2019-07-04 01:03:47 +0800

    update dotfile

--- sync.sh
+++ sync.sh
@@ -7,9 +7,12 @@ do
 	echo `date` &>> sync.info
 
 	git pull &>> sync.info
+	echo 'pull finish \n ready to commit'
 	git add --all
 	git commit -m "update dotfile" &>> sync.info
+	echo 'commit finish \n ready to push'
 	git push &>> sync.info
+	echo 'push finish'
 
 	echo 'finish\n\n\n' &>> sync.info
 	sleep 3600
